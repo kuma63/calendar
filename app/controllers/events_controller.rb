25ce5cf636dd6667c44e9b219150573ec0baa732
kuma63 <t09min25.y@gmail.com>
2022-08-30 14:55:21 +0000

    [add]create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,17 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    event = Event.new(event_params)
+    event.save
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+     params.require(:event).permit(:title, :body, :start_time, :end_time)
+  end
+
+
 end
